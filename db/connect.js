bc3156637fd7979d8ab9248ec3bda81d6ce9e921
Tahabz <etahabaz@gmail.com>
2021-06-17 21:46:58 +0100

    add some dev deps

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const connect = () => mongoose
-	.connect(process.env.DB_CONNECTION, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-})
-.then(() => console.log("Mongo successfully connected"))
+const connect = () =>
+  mongoose
+    .connect(process.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log('Mongo successfully connected'));
 
 module.exports = connect;
--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const morgan = require("morgan");
-const connect = require("./db/connect");
+const express = require('express');
+const morgan = require('morgan');
+const connect = require('./db/connect');
 
 require('dotenv').config();
 const app = express();
 
-
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.get('/', (req, res) => {
-	res.send("hello world");
-
-})
+  res.send('hello world');
+});
 
 const start = async () => {
-	try {
-		await connect();
-		app.listen(process.env.PORT, () => {
-			console.log("Server started at port", process.env.PORT);
-		});
-	} catch (e) {
-		console.error(e);
-	}
-}
+  try {
+    await connect();
+    app.listen(process.env.PORT, () => {
+      console.log('Server started at port', process.env.PORT);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 start();
